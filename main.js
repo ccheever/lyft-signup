6ad4bec275c91fe199e2642471de8721d6e255d1
Charlie Cheever <ccheever@gmail.com>
2017-02-22 15:07:10 -0800

    Added a go to location settings screen

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ import React from 'react';
 import {
   Dimensions,
   Image,
+  Linking,
   StatusBar,
   StyleSheet,
   Text,
@@ -11,9 +12,11 @@ import {
   View,
 } from 'react-native';
 
+import delay from 'delay';
 import ResponsiveImage from '@exponent/react-native-responsive-image';
 
 var {height, width} = Dimensions.get('window');
+console.log({height, width});
 
 const PINK = '#ff00fe';
 
@@ -43,6 +46,9 @@ class AppContainer extends React.Component {
       Exponent.Asset.fromModule(require('./assets/intro_video.mov')).downloadAsync(),
       Exponent.Asset.fromModule(require('./assets/Onboarding - Location - Background@2x.png')).downloadAsync(),
       Exponent.Asset.fromModule(require('./assets/Onboarding - Location - Background@3x.png')).downloadAsync(),
+      Exponent.Asset.fromModule(require('./assets/Onboarding - Location - Background blurred@2x.png')).downloadAsync(),
+      Exponent.Asset.fromModule(require('./assets/Onboarding - Location - Background blurred@3x.png')).downloadAsync(),
+      Exponent.Asset.fromModule(require('./assets/Onboarding - Location - Arrow@2x.png')).downloadAsync(),
     ]);
 
     this.setState({appIsReady: true});
@@ -58,20 +64,103 @@ class LocationPermissionScreen extends React.Component {
     },
   };
 
+  state = {
+    askForLocation: false,
+  };
+
+  async componentDidMount() {
+    const { navigate } = this.props.navigation;
+    // this.setState({askForLocation: true});
+    let permsResult = await Exponent.Permissions.askAsync(Exponent.Permissions.LOCATION);
+    if (permsResult !== 'granted') {
+      console.log("granted");
+    } else {
+      console.log("not granted");
+    }
+    // await delay(500);
+  }
+
   render() {
-    return (
-      <ResponsiveImage
-        sources={{
-          2: require('./assets/Onboarding - Location - Background@2x.png'),
-          3: require('./assets/Onboarding - Location - Background@3x.png'),
-        }}
-        style={{
-          flex: 1,
-          width,
-          height,
-        }}
-      />
-    );
+    const { navigate } = this.props.navigation;
+
+    // if (this.state.askForLocation) {
+    if (true) {
+      return (
+        <View style={{
+            height,
+            width,
+            flexDirection: 'column',
+          }}>
+          <View style={{
+              flex: 1,
+            }}>
+            <ResponsiveImage
+              sources={{
+                2: require('./assets/Onboarding - Location - Background blurred@2x.png'),
+                3: require('./assets/Onboarding - Location - Background blurred@3x.png'),
+              }}
+              style={{
+                position: 'absolute',
+                height,
+                width,
+                top: 0,
+                left: 0,
+              }}
+            />
+          <View style={{
+              alignItems: 'center',
+              justifyContent: 'center',
+              flex: 1,
+          }}>
+              <View style={{
+                  height: 184, //(256 / 1024 * height), // / 2,
+                  width: 272, //(376 / 576) * width, // 378 / 2,
+                  backgroundColor: 'transparent',
+              }}>
+                <Image source={require('./assets/Onboarding - Location - Arrow@2x.png')} style={{
+                    width: 84 / 2,
+                    height: 144 / 2,
+                    position: 'absolute',
+                    top: 184,
+                    marginTop: 15 + 30,
+                    marginLeft: 272 / 4 - (84 / 2 / 2),
+                    left: 272 / 2,
+                }} />
+              </View>
+            </View>
+          </View>
+          <View style={{
+              backgroundColor: 'transparent',
+              marginBottom: 40 / 2.5,
+              marginHorizontal: 40 / 2.5,
+          }}>
+            <Text style={{
+                justifyContent: 'center',
+                alignItems: 'center',
+                textAlign: 'center',
+                fontSize: 21,
+                fontWeight: 'bold',
+                color: 'white',
+            }}>Your location is needed to find nearby drivers</Text>
+          </View>
+        </View>
+
+      );
+    } else {
+      return (
+        <ResponsiveImage
+          sources={{
+            2: require('./assets/Onboarding - Location - Background@2x.png'),
+            3: require('./assets/Onboarding - Location - Background@3x.png'),
+          }}
+          style={{
+            flex: 1,
+            width,
+            height,
+          }}
+        />
+      );
+    }
   }
 }
 
@@ -161,11 +250,89 @@ class InitialScreen extends React.Component {
   }
 }
 
+class GoToSettingsScreen extends React.Component {
+  static navigationOptions = {
+    header: {
+      visible: false,
+    },
+  };
+
+  render() {
+    const { navigate } = this.props.navigation;
+    return (
+      <View style={{
+          height,
+          width,
+          flex: 1,
+          flexDirection: 'column',
+          backgroundColor: 'white',
+        }}>
+        <View style={{
+            marginTop: 22,
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+          <Text style={{
+              fontSize: 22,
+              color: 'black',
+              fontWeight: '600',
+              textAlign: 'center',
+              marginBottom: 14,
+              marginTop: 14,
+            }}>Turn on Location</Text>
+          <Text style={{
+              color: '#333333',
+              fontSize: 13,
+              textAlign: 'center',
+            }}>We can't pick you up if we don't know where you are! Go to Settings, select 'Privacy', then 'Location Services'</Text>
+        </View>
+        <View style={{
+            flex: 1,
+            width,
+            justifyContent: 'flex-end',
+            alignItems: 'center',
+        }}>
+          <Image
+            source={require('./assets/Onboarding - Location - Settings@2x.png')}
+            style={{
+              width: 523 / 2,
+              height: 854 / 2,
+            }}
+          />
+        </View>
+        <TouchableOpacity
+          onPress={() => {
+            console.log("Touched that settings thing");
+            // iOS only :/
+            Linking.openURL("prefs:root=General");
+          }}
+          activeOpacity={0.8}
+        >
+          <View style={{
+              backgroundColor: '#FF00BB',
+              justifyContent: 'center',
+              alignItems: 'center',
+              height: 66,
+              width,
+          }}>
+            <Text style={{
+                color: 'white',
+                fontSize: 24,
+            }}>Go to 'Settings'</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+
+}
+
 import {
   StackNavigator,
 } from 'react-navigation';
 
 const BasicApp = StackNavigator({
+  GoToSettingsScreen: {screen: GoToSettingsScreen},
   InitialScreen: {screen: InitialScreen},
   LocationPermissionScreen: {screen: LocationPermissionScreen},
 }, {
